b6cbaee30d5f4cd4b90952e207f2376fdb993920
Mykhailo Matsehora <m.matsehora@pin-up.team>
2020-07-27 19:00:24 +0300

    fix accordeon

--- src/modules/accordeon.js
+++ src/modules/accordeon.js
@@ -1,3 +1,5 @@
+
+
 const accordeon = () => {
 
     const accordionOne = document.getElementById('accordion'),
@@ -5,38 +7,45 @@ const accordeon = () => {
         panelHeadFirst = accordionOne.querySelectorAll('.panel-heading'),
         panelHeadSecond = accordionTwo.querySelectorAll('.panel-heading'),
         collapseFirst = accordionOne.querySelectorAll('.collapse'),
+        btn = document.querySelectorAll('.button'),
         collapseSecond = accordionTwo.querySelectorAll('.collapse');
 
     panelHeadFirst.forEach((item, i) => {
-        item.addEventListener('click', (event) => {
+        item.addEventListener('click', handleItemClick(collapseFirst, i));
+    });
+
+    panelHeadSecond.forEach((item, i) => {
+        item.addEventListener('click', handleItemClick(collapseSecond, i));
+    });
+
+    btn.forEach((item) => {
+        item.addEventListener('click' , (event) => {
             event.preventDefault();
-            let target = event.target;
 
             collapseFirst.forEach((elem) => {
                 elem.classList.remove('in');
             });
 
-            if (target.closest('.panel-heading')) {
-                collapseFirst[i].classList.add('in');
-            }
+            item.closest('.panel').nextElementSibling.children[1].classList.add('in');
+            
         });
     });
 
-    panelHeadSecond.forEach((item, i) => {
-
-        item.addEventListener('click', (event) => {
+    function handleItemClick(collapseBlock, i) {
+        return (event) => {
             event.preventDefault();
             let target = event.target;
 
-            collapseSecond.forEach((item) => {
-                item.classList.remove('in');
+            collapseBlock.forEach((elem) => {
+                elem.classList.remove('in');
             });
 
             if (target.closest('.panel-heading')) {
-                collapseSecond[i].classList.add('in');
+                collapseBlock[i].classList.add('in');
             }
-        });
-    });
+        };
+    }
+
 };
 
 export default accordeon;
